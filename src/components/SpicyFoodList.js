d32b73557f8d7aa75ee5750c2d27f2fab36f03ec
gizem <gizemc.deniz@gmail.com>
2022-03-11 17:26:19 +0300

    done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,24 +3,49 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+const [filterBy, setFilterBy] = useState("All");
+
+const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+}); 
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
-  }
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+  } 
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  } 
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
-  ));
-
+  )); 
+  
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
-  );
+  ); 
+
 }
 
 export default SpicyFoodList;
